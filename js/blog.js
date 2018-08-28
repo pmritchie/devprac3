2e32005bc4bab9ec9bf5f71c87621717aea48804
Patrick M Ritchie <pmritchie@student.fullsail.edu>
2018-08-28 17:53:48 -0400

    Initial JS

--- js/blog.js
+++ js/blog.js
@@ -1,1 +1,24 @@
-document.querySelector("#blog h2").innerHTML = "New Text";
+document.querySelector("#blog h2").innerHTML = "Camping";
+
+var xhr = new XMLHttpRequest();
+xhr.onload = function(){
+
+	var data = JSON.parse(xhr.responseText);
+
+	var locationSection = document.querySelector('#blog');
+	if(locationSection){
+		var locations ='';
+		for(var i = 0; i<data.locations.length; i++){
+			locations += '<article>';
+			locatoins += '<p class="thumbnail"><img src="'+ data.locations[i].image +'"alt="'+ data.locations[i].location +' "></p> ';
+			locations += '<h3>' + data.locations[i].location + '</h3>';
+			location == '<h4 class="location">' + data.locations[i].city + ', '
+
+			locations += '</article>';
+		}
+		locationSection.querySelector('h2').insertAdjacentHTML('afterend', locations);
+	}
+}
+
+xhr.open('Get', '', true);
+xhr.send(null);
