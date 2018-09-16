8e5e4aa7f618b6ebd99ed290d324ac5e96e76476
Patrick M Ritchie <pmritchie@student.fullsail.edu>
2018-09-16 19:19:01 -0400

    Header/Paragraph/Image change

---
+++ Assignment_3/js/form.js
@@ -0,0 +1,12 @@
+var contactForm = document.querySelector('#form h2');
+contactForm.innerHTML = "Contact Me";
+//document.querySelector("#form h2").innerHTML = "Contact Me";
+
+var formParagraph = document.querySelectorAll('#form p')
+formParagraph[0].innerHTML = "Lets go camping! Enter your information to get updates on events for your camping trip!"
+document.body.style.backgroundImage = "url('images/lighthouse.jpg')";
+//"Lets go camping! Enter your information to get updates on events for your camping trip!";
+
+
+
+
