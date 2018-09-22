99ce5aac12f6cea290a25fedc5d02c00c554903b
Patrick M Ritchie <pmritchie@student.fullsail.edu>
2018-09-22 15:58:08 -0400

    Button and Modal Update

--- Portfolio3_Prototype/js/blog.js
+++ Portfolio3_Prototype/js/blog.js
@@ -43,56 +43,47 @@ xhr.send(null)
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById('myBtn');
+var btn = document.getElementById('getWeather');
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// btn.onclick = function() {
-//     modal.style.display = "block";
-// }
+btn.onclick = function() {
+    modal.style.display = "block";
+	 
 
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
-function test(){
-	console.log("It works");
-	var choice = document.querySelectorAll('#blog a');
-	if(choice[0]){
-		console.log("0");
-	}
-	else if(choice[1]){
-		console.log("1");
-	}
-	else if(choice[2]){
-		console.log("2")
-	}
+    	var weather1 = fetch('https://api.openweathermap.org/data/2.5/weather?q=Key Largo,us&APPID=f23ce7330ba3f39eaafdde4c3e827470')
+		.then((response)=> response.json()).then((data)=> console.log(data.main));
+
+		// var weather2 = fetch('https://api.openweathermap.org/data/2.5/weather?q=Cedar Creek,us&APPID=f23ce7330ba3f39eaafdde4c3e827470')
+		// .then((response)=> response.json()).then((data)=> console.log(data));
+
+		// var weather3 = fetch('https://api.openweathermap.org/data/2.5/weather?q=Chattahoochee,us&APPID=f23ce7330ba3f39eaafdde4c3e827470')
+		// .then((response)=> response.json()).then((data)=> console.log(data));
+
+document.getElementsbyTagName("p")[0].innerHTML= data.main
+
 
 }
-function drop() {
-    document.getElementById("myDropdown").classList.toggle("show");
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
 }
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-        	}
-	    }
+window.onclick = function(event){
+	if(event.target == modal) {
+		modal.style.display = "none";
 	}
 }
 
-// When the user clicks on the button, open the modal 
-// function getWeather(event){
-// 	fetch('http://www.weather.gov/forecasts/xml/sample_products/browser_interface/ndfdBrowserClientByDay.php?zipCodeList=32625&format=24+hourly&startDate=2009-04-21&numDays=7')
 
-// }
-// var click = document.querySelector('#myBtn');
-// click.addEventListener('click', getWeather);
-// document.getElementById('selectionOne').addEventListener('click', getWeather);
+
+
+
+
+
+
+
+
+
